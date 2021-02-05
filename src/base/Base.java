980448a380cd17ad620a29431d49e45f21fb0b89
ggauravs <ggauravs@GGAURAVS-IN.in.oracle.com>
2021-02-05 18:48:46 +0530

    new

--- src/base/Base.java
+++ src/base/Base.java
@@ -1,22 +1,49 @@
 package base;
 
+import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Base {
+
+	public ResourceBundle rb;
+	public WebDriver driver;
 	
-	public static WebDriver driver;
-	
-		public void Initialize() {
-		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		driver.get("https://www.zoopla.co.uk/");
+	public Base()
+	{
+		rb= ResourceBundle.getBundle("config");
+		
 	}
-	
+	public void init()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		driver= new ChromeDriver();
+		/*try {
+			if(rb.getString("browser").equalsIgnoreCase("chrome"))
+			{
+				System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+				driver= new ChromeDriver();
+			}
+			else if(rb.getString("browser").equalsIgnoreCase("FF"))
+			{
+				System.setProperty("Webdriver.gecko.driver","./Driver/geckodriver.exe");
+				driver= new FirefoxDriver();
+			}
+			else 
+			{
+				System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+				driver= new ChromeDriver();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("error in loading webdriver !! setProperty");
+		}
+		*/
+		driver.manage().timeouts().pageLoadTimeout((Integer.parseInt(rb.getString("Pageloadtime"))), TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait((Integer.parseInt(rb.getString("implicitwait"))), TimeUnit.SECONDS);
+		driver.get(rb.getString("url"));
+	}
 }
